b409ed532574c245ce8d2f04e7989aa3c634b4c1
ibenito <ivan.benito@gmail.com>
2016-04-22 20:50:54 +0200

    Tests

--- src/test/java/rocks/mylink/example/DemoApplicationTests.java
+++ src/test/java/rocks/mylink/example/DemoApplicationTests.java
@@ -1,18 +1,42 @@
 package rocks.mylink.example;
 
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+//import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import rocks.mylink.example.controller.HelloController;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = DemoApplication.class)
 @WebAppConfiguration
 public class DemoApplicationTests {
 
-	@Test
-	public void contextLoads() {
+	private MockMvc mvc;
+
+	public void setUp() throws Exception {
+		mvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
 	}
+
+	@Test
+	public void contextLoads() throws Exception {
+		/*
+		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk())
+			.andExpect(content().string(equalTo("Hello Spring Boot")));
+		*/
+	}
+
+
 
 }
